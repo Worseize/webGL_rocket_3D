b556291849d21dde84adee5a4ffc34b3de606097
Worseize <worseize@gmail.com>
2018-01-17 15:51:28 +0200

    v 1.0.0
    Release

--- graphics.js
+++ graphics.js
@@ -1,35 +1,88 @@
 new p5();
-let angle = 0.01, graphics, mouseIsPressed , start = false;
+let graphics, time = 0, rocket;
+
+function preload(){
+  rocket1 = loadImage('img/rocket1.png');
+}
+window.onresize = function(){
+  createCanvas(innerWidth - 30, innerHeight - 30, WEBGL);
+}
 function setup(){
-  createCanvas(400, 400, WEBGL);
-  graphics = createGraphics(200, 200);
-  graphics.background(255);
+  createCanvas(innerWidth - 30, innerHeight - 30, WEBGL);
 }
 
-
-mouseDragged = function(){
-    graphics.stroke(0, 0, 0);
-    graphics.strokeWeight(3);
-
-    graphics.point(mouseX, mouseY );
-}
-keyPressed = function(){
-  if(keyCode == ENTER){
-    start = true;
-  }else if(keyCode == ESCAPE){
-    start = false;
-  }
-}
 function draw(){
   background(0);
-  ambientLight(100);
-  directionalLight(255, 255, 255, 0, 0, 1);
-  rotateX(angle);
-  rotateY(angle);
-  rotateZ(angle);
-  texture(graphics);
-  box(200);
-  if(start){
-     angle+=0.001;
-  }
+  //ambientLight(255);
+  directionalLight(255, 255, 255, 0, 1, 1);
+  texture(rocket1);
+  //rotateX(time/180 * PI);
+  //rotateY(time/180 * PI);
+  rotateZ(time/180 * PI);
+  showShip();
+  camera(0,0,500,0,0,0,0,1,0)
+  time++
+}
+
+function showShip(){
+  translate(200,0,0);
+  push();
+    translate(0, -10 + (20 + 100 + 10) / 2 , 0) // move whole ship to origin
+    //body + cap
+    cone(10,20);
+    push();
+      translate(0, -60, 0);
+      cylinder(10, 100);
+      translate(0, -50, 0);
+      cone(10,10)
+    pop();
+
+    //fuel 1
+    push()
+      translate(10, -75,10);
+      cone(5,10);
+      push();
+        translate(0, -20, 0);
+        cylinder(5, 30);
+        translate(0,-15,0);
+        cone(3, 10);
+      pop();
+    pop();
+
+    //fuel 2
+    push()
+      translate(10, -75,-10);
+      cone(5,10);
+      push();
+        translate(0, -20, 0);
+        cylinder(5, 30);
+        translate(0,-15,0);
+        cone(3, 10);
+      pop();
+    pop();
+
+    //fuel 3
+    push()
+      translate(-10, -75,-10);
+      cone(5,10);
+      push();
+        translate(0, -20, 0);
+        cylinder(5, 30);
+        translate(0,-15,0);
+        cone(3, 10);
+      pop();
+    pop();
+
+    //fuel 4
+    push()
+      translate(-10, -75,10);
+      cone(5,10);
+      push();
+        translate(0, -20, 0);
+        cylinder(5, 30);
+        translate(0,-15,0);
+        cone(3, 10);
+      pop();
+    pop();
+  pop();
 }
